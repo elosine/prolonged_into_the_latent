f9b5ac647ece3c8a67d06ccd0ab0874b3089c8c3
elosine <studiotttguttt>
2019-11-18 16:04:16 -0500

    uu

--- public/usefulFunctions.js
+++ public/usefulFunctions.js
@@ -79,3 +79,96 @@ function palindromeTimeContainers(dur, minval, maxval, pctmin, pctmax) {
   }
   return timeCont;
 }
+// FUNCTION: array3dtoString -----------------------------------
+function array3dtoString(arr) {
+  var tempstr = "";
+  for (var i = 0; i < arr.length; i++) {
+    var tempstr1 = "";
+    for (var j = 0; j < arr[i].length; j++) {
+      var tempstr2 = "";
+      for (var k = 0; k < arr[i][j].length; k++) {
+        if (k == 0) {
+          tempstr2 = arr[i][j][k].toString();
+        } else {
+          tempstr2 = tempstr2 + "&" + arr[i][j][k].toString();
+        }
+      }
+      if (j == 0) {
+        tempstr1 = tempstr2;
+      } else {
+        tempstr1 = tempstr1 + ";" + tempstr2;
+      }
+    }
+    if (i == 0) {
+      tempstr = tempstr1;
+    } else {
+      tempstr = tempstr + ":" + tempstr1;
+    }
+  }
+  return tempstr;
+}
+// FUNCTION: sortFunction2DArray -----------------------------------
+//use like this: array.sort(sortFunction2DArray)
+function sortFunction2DArray(a, b) {
+  if (a[0] === b[0]) {
+    return 0;
+  } else {
+    //change a[0] < b[0] to a[1] < b[1] to sort by second column etc
+    return (a[0] < b[0]) ? -1 : 1;
+  }
+}
+// FUNCTION: findIndicesOfMax -----------------------------------
+function findIndicesOfMax(inp, count) {
+  var outp = [];
+  for (var i = 0; i < inp.length; i++) {
+    outp.push(i); // add index to output array
+    if (outp.length > count) {
+      outp.sort(function(a, b) {
+        return inp[b] - inp[a];
+      }); // descending sort the output array
+      outp.pop(); // remove the last index (index of smallest element in output array)
+    }
+  }
+  return outp;
+}
+// FUNCTION: downloadStrToHD -----------------------------------
+// download('the content of the file', 'filename.txt', 'text/plain');
+function downloadStrToHD(strData, strFileName, strMimeType) {
+  var D = document,
+    A = arguments,
+    a = D.createElement("a"),
+    d = A[0],
+    n = A[1],
+    t = A[2] || "text/plain";
+
+  //build download link:
+  a.href = "data:" + strMimeType + "charset=utf-8," + escape(strData);
+
+  if (window.MSBlobBuilder) { // IE10
+    var bb = new MSBlobBuilder();
+    bb.append(strData);
+    return navigator.msSaveBlob(bb, strFileName);
+  } /* end if(window.MSBlobBuilder) */
+
+  if ('download' in a) { //FF20, CH19
+    a.setAttribute("download", n);
+    a.innerHTML = "downloading...";
+    D.body.appendChild(a);
+    setTimeout(function() {
+      var e = D.createEvent("MouseEvents");
+      e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      a.dispatchEvent(e);
+      D.body.removeChild(a);
+    }, 66);
+    return true;
+  }; /* end if('download' in a) */
+
+  //do iframe dataURL download: (older W3)
+  var f = D.createElement("iframe");
+  D.body.appendChild(f);
+  f.src = "data:" + (A[2] ? A[2] : "application/octet-stream") + (window.btoa ? ";base64" : "") + "," + (window.btoa ? window.btoa : escape)(strData);
+  setTimeout(function() {
+    D.body.removeChild(f);
+  }, 333);
+  return true;
+}
